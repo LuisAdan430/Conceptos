7ebd83ebf19d3ce0c89ada3aa5883994be581f4e
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-03-30 19:28:54 -0600

    Conceptos

---
+++ Informacion/TiposInyeccionDependecias/InyeccionPorAtributo.java
@@ -0,0 +1,16 @@
+/*
+    TODO :  Inyección por Atributo o Campo (Field Injection)
+    TODO :  Se inyecta la dependencia directamente en un atributo utilizando anotaciones. Es la forma más sencilla,
+    TODO :  pero no es recomendable porque dificulta la prueba unitaria y reduce la encapsulación.
+    TODO :  Ejemplo en Java con Spring Boot:
+*/
+
+@Service
+public class Cliente {
+    @Autowired
+    private Servicio servicio;  // *  Inyección directa en el campo
+
+    public void realizarAccion() {
+        servicio.ejecutar();
+    }
+}
