7bc0f9bed84d7670c66216b0f9602b6472fd3252
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-02 23:30:12 -0600

    Patron de diseño singleton

---
+++ Informacion/Anotaciones/Patrones de Diseño/Singleton.java
@@ -0,0 +1,11 @@
+/*
+    &  Singleton
+    &  Patrón: Singleton
+    &  Descripción: Se asegura de que solo haya una instancia de un objeto en toda la aplicación.
+    &  Ejemplo en Spring Boot:
+*/ 
+@Component
+public class MiComponente {
+    // & Spring crea una única instancia de este bean por defecto.
+}
+// &  Uso en Spring: Por defecto, todos los beans de Spring son Singletons.
