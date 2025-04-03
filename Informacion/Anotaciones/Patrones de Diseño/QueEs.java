d1bfa6c5dcdd3e1f45ec2f3c66b6f4c836e089eb
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-02 23:24:43 -0600

    Tipos de Patrones de diseño

---
+++ Informacion/Anotaciones/Patrones de Diseño/QueEs.java
@@ -0,0 +1,18 @@
+/*
+    * Spring Framework admite e implementa varios patrones de diseño para mejorar la modularidad,
+    * escalabilidad y mantenibilidad del código. Aquí tienes una lista de los patrones más utilizados en Spring y cómo se aplican:
+    * Inversión de Control (IoC) y Dependency Injection (DI)
+    * Singleton
+    * Factory Method
+    * Proxy
+    * Adapter
+    * Template Method
+    * Observer
+    * MVC (Model-View-Controller)
+    * Strategy
+    
+    * Spring implementa varios patrones de diseño para facilitar el desarrollo, mejorar la modularidad y
+    * permitir el desacoplamiento de componentes. Los más importantes son Dependency Injection,
+    * Singleton, Factory Method, Proxy y MVC, pero también encontramos Adapter, Observer y Strategy en
+    * diversos módulos como Spring Security, Spring Data y Spring AOP.
+*/
