a97e3a8f882136d090392603c39592f08c93f115
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-04 23:09:05 -0600

    @PreAuthorize

---
+++ Informacion/Anotaciones/PreAuthorize/@PreAuthorize.java
@@ -0,0 +1,6 @@
+/*
+    * @PreAuthorize("hasRole('ADMIN')")
+    * Uso: Restringe el acceso a un método antes de su ejecución.
+    * Pertenece a:
+*/
+org.springframework.security.access.prepost.PreAuthorize
