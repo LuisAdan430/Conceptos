1d18f1283568473ad6d3d46e078d5f9889d1f472
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-04 23:04:09 -0600

    EnabledGlobalMethodSecurity

---
+++ Informacion/Anotaciones/Configuracion/@EnableGlobalMethodSecurity.java
@@ -0,0 +1,9 @@
+/*
+    * @EnableGlobalMethodSecurity(prePostEnabled = true)
+    * Uso: Permite usar anotaciones como @PreAuthorize y @PostAuthorize.
+    * Pertenece a:
+*/
+org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+/*
+    * Nota: Esta anotación fue reemplazada en Spring Security 6 por @EnableMethodSecurity.
+*/
