a7249caa84f16450d22db31a0c71e2983da89b2f
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-04 23:26:51 -0600

    AuthenticationPrincipal

---
+++ Informacion/Anotaciones/AuthenticationPrincipal/@AuthenticationPrincipal.java
@@ -0,0 +1,7 @@
+/*
+    * @AuthenticationPrincipal
+    * Uso: Inyecta el objeto del usuario autenticado directamente en un método.
+    * Pertenece a:
+*/
+org.springframework.security.core.annotation.AuthenticationPrincipal
+
