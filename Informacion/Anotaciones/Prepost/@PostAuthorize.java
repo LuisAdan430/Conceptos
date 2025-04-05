8f8db589594bccf55a8e1a211ebe40035daa5d6a
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-04 23:12:38 -0600

    @PostAuthorize

---
+++ Informacion/Anotaciones/Prepost/@PostAuthorize.java
@@ -0,0 +1,7 @@
+/*
+    * @PostAuthorize("returnObject.owner == authentication.name")
+    * Uso: Evalúa la expresión después de la ejecución del método.
+    * Pertenece a:
+*/
+org.springframework.security.access.prepost.PostAuthorize
+
