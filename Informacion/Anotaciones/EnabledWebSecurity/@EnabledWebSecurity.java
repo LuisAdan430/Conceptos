a0855a043056dae032aa7268189b659ed6b057fd
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-04 22:55:58 -0600

    @EnabledWebSecutiry

---
+++ Informacion/Anotaciones/EnabledWebSecurity/@EnabledWebSecurity.java
@@ -0,0 +1,6 @@
+/*
+    * @EnableWebSecurity
+    * Uso: Habilita la configuración de seguridad web personalizada.
+    * Pertenece a:
+*/
+org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
