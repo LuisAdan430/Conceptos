723949155da35653a65c3ba3f001ab74a4cc3e6f
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-04 23:06:00 -0600

    @EnabledMethodSecurity

---
+++ Informacion/Anotaciones/Configuracion/@EnabledMethodSecurity.java
@@ -0,0 +1,6 @@
+/*
+    * @EnableMethodSecurity (nuevo en Spring Security 6)
+    * Uso: Activa la seguridad basada en métodos, reemplazo moderno de @EnableGlobalMethodSecurity.
+    * Pertenece a:
+*/
+org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
