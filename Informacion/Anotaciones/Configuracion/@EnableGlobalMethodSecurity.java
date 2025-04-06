3a469c3cee37bff42ab01d8f6603757ce5ecb772
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-05 23:53:21 -0600

    Eliminacion de anotacion repetida

--- Informacion/Anotaciones/Configuracion/@EnableGlobalMethodSecurity.java
+++
@@ -1,9 +0,0 @@
-/*
-    * @EnableGlobalMethodSecurity(prePostEnabled = true)
-    * Uso: Permite usar anotaciones como @PreAuthorize y @PostAuthorize.
-    * Pertenece a:
-*/
-org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
-/*
-    * Nota: Esta anotación fue reemplazada en Spring Security 6 por @EnableMethodSecurity.
-*/
