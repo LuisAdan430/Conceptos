7c6e16ed92a0927e9a2f1785004a381e046f7bc9
LuisAdan430 <luisangelcanalescalva@gmail.com>
2025-04-20 01:45:56 -0600

    Info: ¿Que es un patron de diseño?

---
+++ Informacion/Anotaciones/Patrones de Diseño/QueEsUnPatronDeDiseño.java
@@ -0,0 +1,27 @@
+/*
+    * Los patrones de diseño (design patterns) son soluciones habituales a
+    * problemas comunes en el diseño de software. Cada patrón es como un
+    * plano que se puede personalizar para resolver un problema de diseño particular de tu código.
+    
+    * Los patrones de diseño son soluciones habituales a problemas que ocurren con frecuencia en el diseño de software. 
+    * Son como planos prefabricados que se pueden personalizar para resolver un problema de diseño recurrente en tu código.
+    * No se puede elegir un patrón y copiarlo en el programa como si se tratara de funciones o bibliotecas ya preparadas. 
+    * El patrón no es una porción específica de código, sino un concepto general para resolver un problema particular. 
+    * Puedes seguir los detalles del patrón e implementar una solución que encaje con las realidades de tu propio programa.
+    * A menudo los patrones se confunden con algoritmos porque ambos conceptos describen soluciones típicas a problemas conocidos. 
+    * Mientras que un algoritmo siempre define un grupo claro de acciones para lograr un objetivo, un patrón es una descripción de más alto nivel de una solución.
+    * El código del mismo patrón aplicado a dos programas distintos puede ser diferente.
+    
+    * Una analogía de un algoritmo sería una receta de cocina: ambos cuentan con pasos claros para alcanzar una meta. 
+    * Por su parte, un patrón es más similar a un plano, ya que puedes observar cómo son su resultado y sus funciones,
+    * pero el orden exacto de la implementación depende de ti.
+    
+    * ¿En qué consiste el patrón?
+    * La mayoría de los patrones se describe con mucha formalidad para que la gente pueda reproducirlos en muchos contextos.
+    * Aquí tienes las secciones que suelen estar presentes en la descripción de un patrón:
+    * El propósito del patrón explica brevemente el problema y la solución.
+    * La motivación explica en más detalle el problema y la solución que brinda el patrón.
+    * La estructura de las clases muestra cada una de las partes del patrón y el modo en que se relacionan.
+    * El ejemplo de código en uno de los lenguajes de programación populares facilita la asimilación de la idea que se esconde tras el patrón.
+    * 
+*/
